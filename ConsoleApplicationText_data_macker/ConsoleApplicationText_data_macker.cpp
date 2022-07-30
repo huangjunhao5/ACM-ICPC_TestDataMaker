81d03ff80ddbb5e4f2d649d4018d4e035ae4140b
黄俊豪 <1814155869@qq.com>
2022-07-30 22:53:01 +0800

    示例代码

--- ConsoleApplicationText_data_macker/ConsoleApplicationText_data_macker.cpp
+++ ConsoleApplicationText_data_macker/ConsoleApplicationText_data_macker.cpp
@@ -15,10 +15,11 @@ using namespace std;
 random_device rd;
 mt19937 mt(rd());
 
-
+//用于生成数据的函数：必须为void型，并且要接受一个参数，该参数表示当前生成的数据编号
 void make(__int32 num) {
 	int n = 3e5;
 	cout << n << endl;
+	//可以通过传入的数据编号确定生成什么数据：是否随机生成数据、或者生成确定的什么数据
 	if (num == 1) {
 		for (int i = 1; i <= n; i++) {
 			cout << i << ' ';
@@ -63,10 +64,12 @@ void make(__int32 num) {
 
 signed main() {
 	string stdEXEPath = "D:\\11111\\repos\\ConsoleApplication3\\x64\\Debug\\ConsoleApplication3.exe";
-	string textPath = "D:/input/";
+	//stdEXEPath是标程在电脑上的全路径
+	string testPath = "D:/input/";
+	//testPath是生成数据的全路径
 	__int32 textnum;
 	cin >> textnum;
-	DataMaker* maker = new DataMakerFromEXE(stdEXEPath ,textPath, make, textnum);
+	DataMaker* maker = new DataMakerFromEXE(stdEXEPath ,testPath, make, textnum);
 	maker->run();
 	delete maker;
 	return 0;
